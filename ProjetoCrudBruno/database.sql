b7177b49747c8cadf5abe902d9f60b29703f5582
Bruno Santos Ichikawa <149208859+BrunoIchikawa@users.noreply.github.com>
2024-10-22 15:49:23 -0300

    Update 2
    Foi realizado a criação do listar, onde é listado todos os cadastros registrados feitos, tirando um erro que deixa todos os registros como femininos, esta funcionado perfeitamente, falta criar um botão para voltar ao index e corrigir o cad.html que quando é pressionado o botão de voltar ele da um back.history() e não volta a pagina index

--- ProjetoCrudBruno/database.sql
+++ ProjetoCrudBruno/database.sql
@@ -6,9 +6,9 @@ create table tbusuario(
 	codi_t int primary key auto_increment,
     nome_t varchar(40),
     email_t varchar(40),
-    senha_t varchar(8),
+    senha_t varchar(40),
     sexo_t varchar(1),
-    dtna_t datetime
+    dtna_t date
 );
 
 select * from tbusuario;
